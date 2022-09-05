621be515f3a11f461c995723fbc919de187bde78
ashtrace <0aishwaryaraj@gmail.com>
2022-09-05 20:44:36 +0530

    Fixed #error config.h must be included before this header

--- includes/loader.cpp
+++ includes/loader.cpp
@@ -1,4 +1,17 @@
-#include <bfd.h>
+#ifndef PACKAGE
+	#define PACKAGE
+		#ifndef PACKAGE_VERSION
+			#define PACKAGE_VERSION
+			#include <bfd.h>
+		#undef PACKAGE_VERSION
+		#else
+			#include <bfd.h>
+		#endif
+	#undef PACKAGE
+#else
+	#include <bfd.h>
+#endif
+
 #include "loader.hpp"
 
 /* FUNCTION: open_bfd
